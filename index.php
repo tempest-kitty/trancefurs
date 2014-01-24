8c2df7ff1cae3982de017d288710be6e3c0ab6a2
Kanibal Thorne <mcor4472@gmail.com>
2014-01-24 19:49:46 +0000

    Bug fixes and patches
    Fixed the fact that 0.4.0 was totally broken

--- index.php
+++ index.php
@@ -1,38 +1,32 @@
 <?php
-//Variables and global includes first
-	$error='Whoops! Something went wrong while I was making the page... Uh, help?';
+//Require anything we need
 	require_once('config.php');
-
-//Establish which page we are on and if none then we are home
-	if( !isset($_GET["page"]) || ($_GET["page"] == '') )
-	{
-		$location=$home;
-	}
-	else
-		$location=$_GET["page"];
-		
-	$currentPage = strip_tags($location);
 	
-//Now lets buid it! Header first with the Navigation Bar inside.
+//Establish where we are
+	if( !isset($_GET["page"]) || ($_GET["page"] == '') ) {
+		$location = 'home';
+	} else {
+		$currentPage = strip_tags($_GET["page"]);
+	};
+
+//Attach the Header
 	require_once($includes.'header.php');
 		
 //Content Section --START--
 	echo '<div id="content">';
-		if($currentPage===$home)
-		{
+		if($currentPage == 'home') {
 			require_once($includes.'home.php');
-		}
-		else
-			if( (!file_exists($includes.$currentPage.'.php')) && (!file_exists($includes.$currentPage.'.html')) )
-			{
-				echo($error);
+		} else {
+			if( (!file_exists($includes.$currentPage.'.php')) && (!file_exists($includes.$currentPage.'.html')) ) {
+				echo('Whoops! Something went wrong while I was making the page... Uh, help?');
 			} else {
 				if(file_exists($includes.$currentPage.'.php')) require_once $includes.$currentPage.'.php';
 				if(file_exists($includes.$currentPage.'.html')) require_once $includes.$currentPage.'.html';
-			}			
+			};
+		};
 	echo '</div>';
 //Content Section --END--
 
-//And finally lets tack the footer on to the bottom
+//Attach the Footer
 	require_once($includes.'footer.php');
 ?>

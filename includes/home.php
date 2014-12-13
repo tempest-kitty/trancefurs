e7db0a4bf3a077acfa07b2127ac0ab04705f87f5
Kanibal Thorne <mcor4472@gmail.com>
2014-12-13 18:55:52 +0000

    News fix and empty folders fix
    Added the news folder and added templates to it. This fixes #19

--- includes/home.php
+++ includes/home.php
@@ -1,14 +1,9 @@
-<!-- Stats for the main #TranceFurs channel -->
-<div class="sidebar">
-	<?php
-		require_once('stats/toptalls.trancefurs.htm');
-	?>
-</div>
-
 <!-- The about paragraph for TranceFurs.net -->
 <div id="about">
 	<h3>Welcome to TranceFurs</h3>
-	<p>Welcome to TranceFurs. Just lay back and relax, you know why you're hear, slipping nice and deep now...</p>
+	<p>Just lay back and relax, you know why you're hear, slipping nice and deep now...</p>
+	<p>The TranceFurs network is dedicated to providing a safe hangout place for those in the furry community who have an interest in Hypnosis. From just relaxation, to the more kinky side, come and stop past for a chat.</p>
+	<p>TranceFurs Network provides IRC servers, internet radio station, and even game servers for its members to use. We hope you enjoy your stay, (what you may remember of it) and hope you enjoy our services!.</p>
 </div>
 
 <!--News Section-->

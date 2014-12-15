34c9fbabf9cdee89620ea6efac5c736fc1bcb328
Tempest <tempest.kittie@gmail.com>
2014-12-15 19:08:23 +1100

    Analytics
    updated tracking

--- includes/games.php
+++ includes/games.php
@@ -27,3 +27,13 @@ Server: starmade.trancefurs.net:4242
 		<iframe src="https://docs.google.com/forms/d/1HrOHjyQ-GEOyrRiXcmxjJmhb4NE41wG5raNppUtbdEo/viewform?embedded=true" width="600" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46286721-1', 'auto');
+  ga('send', 'pageview');
+
+</script>

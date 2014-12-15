e62466ddaa12245fbd5da3cf262cdcfab5f4f6ec
Tempest <tempest.kittie@gmail.com>
2014-12-15 19:10:07 +1100

    Analytics

--- includes/register.php
+++ includes/register.php
@@ -2,7 +2,18 @@
 <div>
 	<p>When you want to go somewhere new on TranceFurs the first thing you'll probably find is that you can't register your own channel. We have our reasons for this and we will do our very best to still allow you to have your own channels as they're needed on the network.</p>
 	<p>The form to the left will allow us to create your channel or contact you for further discussions. Take the time to fill it in and hopefully we can register your channel on the network in no time</p>
-</div>
+</div>
+
 <div class="highlightbox">
 	<iframe src="https://docs.google.com/forms/d/1Z0vdHVISzy7GjOp0tyaozA3ufQa4_4dqN_M45PvxQqY/viewform?embedded=true" width="760" height="753" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46286721-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
